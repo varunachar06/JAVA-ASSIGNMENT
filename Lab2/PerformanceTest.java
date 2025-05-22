aff36e6496ac55cef0530ef6da5ddf470ef3984a
varunachar06 <varunhmv@gmail.com>
2025-05-22 22:51:37 +0530

    Add files via upload

---
+++ Lab2/PerformanceTest.java
@@ -0,0 +1,18 @@
+public class PerformanceTest{ 
+	 public static void main(String[] args){ 
+	 long startTime = System.currentTimeMillis(); 
+	 StringBuffer sb = new StringBuffer("Java"); 
+	 for (int i=0; i<10000; i++){ 
+	 sb.append("AIET"); 
+	 } 
+	 System.out.println("Time taken by StringBuffer: " + (System.currentTimeMillis() - startTime) + 
+	"ms"); 
+	 startTime = System.currentTimeMillis(); 
+	 StringBuilder sb2 = new StringBuilder("Java"); 
+	 for (int i=0; i<10000; i++){ 
+	 sb2.append("AIET"); 
+	 } 
+	 System.out.println("Time taken by StringBuilder: " + (System.currentTimeMillis() - startTime) + 
+	"ms"); 
+	 } 
+	} 
---
+++ Lab2/README.md
@@ -0,0 +1,5 @@
+MapInterfaceDemo1 output - https://github.com/tarunagarwal1713/Advance-java/blob/main/Lab2/MapInterfaceDemo1.png
+
+MapInterfaceDemo1 output - https://github.com/tarunagarwal1713/Advance-java/blob/main/Lab2/MapInterfaceDemo2.png
+
+PerformanceTest output - https://github.com/tarunagarwal1713/Advance-java/blob/main/Lab2/PerformanceTest.png
